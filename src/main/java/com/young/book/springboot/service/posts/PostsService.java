f4db7962611d03ccc77f95f41baa082581c6e5f4
youngseo <ewa111@naver.com>
2022-04-19 21:57:48 +0900

    4-1. mustache 화면 구성

--- src/main/java/com/young/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/young/book/springboot/service/posts/PostsService.java
@@ -48,4 +48,10 @@ public class PostsService {
         // collect를 사용해서 List로 변환.
     }
 
+    @Transactional
+    public void delete (Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id ));
+        postsRepository.delete(posts);
+    }
+
 }
--- src/main/java/com/young/book/springboot/web/IndexController.java
+++ src/main/java/com/young/book/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.young.book.springboot.web;
 
 import com.young.book.springboot.service.posts.PostsService;
+import com.young.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -18,12 +20,18 @@ public class IndexController {
         return "index"; // src/main/resources/templates/ + .mustache 로 전환되어 ViewResolver가 처리
     }
 
-
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
     }
 
 
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
 
 }
